035dcc918101669a5914b473010b73e3fb1924e7
arashi walker and Victoria Martinez <arashi@>
2018-10-16 15:19:28 -0700

    add click and toggle feature to each one

---
+++ js/scripts.js
@@ -0,0 +1,90 @@
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $(".init-1").slideToggle();
+
+  });
+
+  $(".clickable2").click(function() {
+    $(".init-2").slideToggle();
+
+  });
+  $(".clickable3").click(function() {
+    $(".init-3").slideToggle();
+
+  });
+  $(".clickable4").click(function() {
+    $(".init-4").slideToggle();
+
+  });
+  $(".clickable5").click(function() {
+    $(".init-5").slideToggle();
+
+  });
+  $(".clickable6").click(function() {
+    $(".init-6").slideToggle();
+
+  });
+  $(".clickable7").click(function() {
+    $(".init-7").slideToggle();
+
+  });
+  $(".clickable8").click(function() {
+    $(".init-8").slideToggle();
+
+  });
+  $(".clickable9").click(function() {
+    $(".init-9").slideToggle();
+
+  });
+  $(".clickable10").click(function() {
+    $(".init-10").slideToggle();
+
+  });
+  $(".clickable11").click(function() {
+    $(".init-11").slideToggle();
+
+  });
+  $(".clickable12").click(function() {
+    $(".init-12").slideToggle();
+
+  });
+  $(".clickable13").click(function() {
+    $(".init-13").slideToggle();
+
+  });
+  $(".clickable14").click(function() {
+    $(".init-14").slideToggle();
+
+  });
+  $(".clickable15").click(function() {
+    $(".init-15").slideToggle();
+
+  });
+  $(".clickable16").click(function() {
+    $(".init-16").slideToggle();
+
+  });
+  $(".clickable17").click(function() {
+    $(".init-17").slideToggle();
+
+  });
+  $(".clickable18").click(function() {
+    $(".init-18").slideToggle();
+
+  });
+  $(".clickable19").click(function() {
+    $(".init-19").slideToggle();
+
+  });
+
+
+
+
+   $("h2").click(function() {
+   $("p").removeClass();
+  $("p").addClass("panel-body");
+   });
+
+
+
+  });
